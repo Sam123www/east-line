e70caf7c05d7c6f7ffcdc58743df2837c18505ad
Sam <75374659+Sam123www@users.noreply.github.com>
2025-03-25 11:35:00 +0800

    test

---
+++ script/eastLine.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import time
+
+const write = 1
+const turnHead = 2
+const minTime = 5
+const maxTime = 15
+static = write
+if(static == write){
+    time.sleep(random.randint(minTime, maxTime))
+    
+}
+elif(static == turnHead){
+
+}
